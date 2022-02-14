a0f58b32c861ba9b003e4951ae01b2173a842d50
Vittorio Allegra <vittorio.allegra@outlook.com>
2022-02-14 22:28:23 +0100

    Replace redux connect with hooks

--- src/app/containers/Home/Home.tsx
+++ src/app/containers/Home/Home.tsx
@@ -1,46 +1,37 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Grid } from '@mui/material';
 import { InfiniteList, LocationCard, Spinner } from '../../components';
-import { IApplicationStore, ILocationList } from '../../interfaces';
+import { IApplicationStore } from '../../interfaces';
 import { appActions } from '../../store';
 import { DEFAULT_LIST_LIMIT } from '../../config';
 
-interface IActionProps {
-    loadPage: typeof appActions.loadPageRequested;
-}
-
-interface IStoreProps {
-    locations: ILocationList;
-    isLoading: boolean;
-    hasError: boolean;
-}
-
-type IProps = IActionProps & IStoreProps;
-
-const Home: React.FC<IProps> = (props) => {
+export const Home: React.FC = () => {
     const [shouldDisplaySpinner, setDisplaySpinner] = React.useState(true);
+    const locations = useSelector((state: IApplicationStore) => state.app.locations);
+    const isLoading = useSelector((state: IApplicationStore) => state.app.isLoading);
+    const dispatch = useDispatch();
 
     React.useEffect(() => {
         setDisplaySpinner(true);
-        props.loadPage(0, DEFAULT_LIST_LIMIT);
+        dispatch(appActions.loadPageRequested(0, DEFAULT_LIST_LIMIT));
     }, []);
 
     const handleLoadNext = () => {
         setDisplaySpinner(false);
-        props.loadPage(props.locations.page + 1, DEFAULT_LIST_LIMIT);
+        dispatch(appActions.loadPageRequested(locations.page + 1, DEFAULT_LIST_LIMIT));
     };
 
     return (
         <div className="home">
-            {props.isLoading && shouldDisplaySpinner && <Spinner />}
+            {isLoading && shouldDisplaySpinner && <Spinner />}
             <div className="home__header">
                 <img alt="logo" src={`${process.env.PUBLIC_URL}/images/logo.svg`} />
             </div>
             <div className="home__content">
                 <InfiniteList
-                    hasMore={props.locations.hasMore}
-                    items={props.locations.items.map((item) => (
+                    hasMore={locations.hasMore}
+                    items={locations.items.map((item) => (
                         <Grid key={item.id} item={true} xs={12} md={4}>
                             <LocationCard location={item} />
                         </Grid>
@@ -51,15 +42,3 @@ const Home: React.FC<IProps> = (props) => {
         </div>
     );
 };
-
-const actions: IActionProps = {
-    loadPage: appActions.loadPageRequested,
-};
-
-const mapStateToProps = (state: IApplicationStore): IStoreProps => ({
-    locations: state.app.locations,
-    isLoading: state.app.isLoading,
-    hasError: state.app.hasError,
-});
-
-export default connect(mapStateToProps, actions)(Home);
